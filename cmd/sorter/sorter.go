b4d51b5e8fcbc2196ac1a23a3f8124dcc0eddb4f
Ilya Edrets <edrets.ilya@gmail.com>
2024-07-19 02:55:55 +0300

    replace tasks by goroutines and channels

--- cmd/sorter/sorter.go
+++ cmd/sorter/sorter.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"merge-sort/internal/chunk"
-	"merge-sort/internal/task"
 	"merge-sort/internal/tracing"
 	"os"
 	"slices"
@@ -25,65 +24,81 @@ func main() {
 	defer tracing.Duration(tracing.Track("main"))
 	args := getArgs()
 
+	chunks := splitIntoChunks(args)
+	sortChunks(chunks)
+	mergeChunks(chunks)
+	chunks[0].Rename(args.outputFile)
+}
+
+func splitIntoChunks(args args) []*chunk.Chunk {
 	inputFile := chunk.NewChunk(args.input)
 	chunks, err := inputFile.SplitIntoChunks(args.outputFolder, args.chunkSize)
 	if err != nil {
 		panic(err)
 	}
 
-	tasks := make([]*task.Task[int], 0)
+	return chunks
+}
+
+func sortChunks(chunks []*chunk.Chunk) {
+	sortChannel := make(chan error)
+
 	for _, chunk := range chunks {
-		tasks = append(tasks, task.Run(func() (int, error) { return 0, chunk.Sort() }))
+		go func() {
+			sortChannel <- chunk.Sort()
+		}()
 	}
 
-	for _, task := range tasks {
-		_, err = task.Await()
+	for i := 0; i < len(chunks); i++ {
+		err := <-sortChannel
 		if err != nil {
 			panic(err)
 		}
 	}
+}
 
-	channel := make(chan mergeResult)
+func mergeChunks(chunks []*chunk.Chunk) {
+	mergeChannel := make(chan mergeResult)
+
 	for len(chunks) > 1 {
 		var nextChunks []*chunk.Chunk
 
 		for i := 0; i < len(chunks); i += 2 {
 			if i+1 < len(chunks) {
-				mergePair(channel, chunks[i], chunks[i+1])
+				go func() {
+					chunk, err := mergePair(chunks[i], chunks[i+1])
+					mergeChannel <- mergeResult{chunk, err}
+				}()
 			} else {
 				nextChunks = append(nextChunks, chunks[i])
 			}
 		}
 
 		for i := 0; i < len(chunks)/2; i++ {
-			mergeResult := <-channel
+			mergeResult := <-mergeChannel
 			if mergeResult.err != nil {
-				panic(err)
+				panic(mergeResult.err)
 			}
 			nextChunks = append(nextChunks, mergeResult.chunk)
 		}
 
 		chunks = nextChunks
 	}
-
-	chunks[0].Rename(args.outputFile)
 }
 
-func mergePair(channel chan mergeResult, chunk1 *chunk.Chunk, chunk2 *chunk.Chunk) {
-	go func() {
-		chunk, err := chunk2.Merge(chunk1)
-		if err != nil {
-			channel <- mergeResult{chunk: chunk, err: err}
-		}
+func mergePair(chunk1 *chunk.Chunk, chunk2 *chunk.Chunk) (*chunk.Chunk, error) {
+	chunk, err := chunk1.Merge(chunk2)
+	if err != nil {
+		return chunk, err
+	}
 
-		err = chunk1.Remove()
-		if err != nil {
-			channel <- mergeResult{chunk: chunk, err: err}
-		}
+	err = chunk1.Remove()
+	if err != nil {
+		return chunk, err
+	}
 
-		err = chunk2.Remove()
-		channel <- mergeResult{chunk: chunk, err: err}
-	}()
+	err = chunk2.Remove()
+	return chunk, err
 }
 
 func getArgs() args {
